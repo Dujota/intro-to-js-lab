0b5e495d60c28e7a1659c212b7cf3e8b874da9e0
Denis S Dujota <denisdujota@gmail.com>
2024-05-26 16:30:33 -0500

    update src folder

--- src/triangleAreaCalculator.js
+++ src/triangleAreaCalculator.js
@@ -1,8 +1,11 @@
-/**
+/** 
  * This function should return the area of a triangle with the given base and height
- * */
-function triangleAreaCalculator(b, h) {}
+ * */ 
+function triangleAreaCalculator(b,h){
+    const a = b * h / 2;
+    return a;
+}
 
-if (typeof module === 'object') {
-  module.exports = triangleAreaCalculator;
+if(typeof module === 'object'){
+    module.exports = triangleAreaCalculator;
 }
--- src/trianglePerimeterCalculator.js
+++ src/trianglePerimeterCalculator.js
@@ -1,8 +1,11 @@
 /**
  * This function should return the perimeter of a triangle given three side lengths
  */
-function trianglePerimeterCalculator(sideOneLength, sideTwoLength, sideThreeLength) {}
+function trianglePerimeterCalculator(sideOneLength, sideTwoLength, sideThreeLength){
+    const perimeter = sideOneLength + sideTwoLength + sideThreeLength;
+    return perimeter;
+}
 
-if (typeof module === 'object') {
-  module.exports = trianglePerimeterCalculator;
+if(typeof module === 'object'){
+    module.exports = trianglePerimeterCalculator;
 }
